242749c060d5e63192c4266d48ea5e90621c6645
saif <saiflotfi@proton.me>
2024-11-05 12:54:09 +0200

    add submit function

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -30,13 +30,20 @@
             required
           ></v-text-field>
         </v-col>
+        <v-col cols="12" md="6">
+          <v-text-field
+            v-model="salary"
+            :rules="salaryRules"
+            label="Salary"
+            required
+          ></v-text-field>
+        </v-col>
       </v-row>
     </v-container>
-    <!-- Centering the Button -->
     <v-container>
       <v-row justify="center">
         <v-col cols="auto">
-          <v-btn class="mt-2" type="submit" block>Submit</v-btn>
+          <v-btn class="mt-2" type="submit" @click.prevent="submitForm" block>Submit</v-btn>
         </v-col>
       </v-row>
     </v-container>
@@ -51,6 +58,7 @@ const valid = ref(false);
 const firstName = ref("");
 const lastName = ref("");
 const email = ref("");
+const salary = ref("");
 
 // Validation rules
 const nameRules = [
@@ -75,6 +83,17 @@ const emailRules = [
   },
 ];
 
+const salaryRules = [
+  (value) => {
+    if (value) return true;
+    return "Salary is required.";
+  },
+  (value) => {
+    if (value > 0) return true;
+    return "Salary should be a number greater than zero.";
+  },
+]
+
 // Function to validate fields (optional)
 const validateFields = () => {
   const nameErrors = nameRules
@@ -83,11 +102,25 @@ const validateFields = () => {
   const emailErrors = emailRules
     .map((rule) => rule(email.value))
     .filter((error) => error !== true);
+  const salaryErrors = emailRules 
+    .map((rule) => rule(salary.value))
+    .filter((error) => error !== true);
 
   return {
     nameErrors,
     emailErrors,
+    salaryErrors
   };
+};
+
+const submitForm = () => {
+  const newEmployee = {
+    firstName: firstName.value,
+    lastName: lastName.value,
+    email: email.value,
+    salary: salary.value
+  };
+  console.log(newEmployee);
 };
 
 // Export the reactive properties for template usage
