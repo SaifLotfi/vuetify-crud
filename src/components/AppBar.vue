9d18de5d710ad8a6b41fa42778af116a75521619
saif <saiflotfi@proton.me>
2024-11-05 11:03:11 +0200

    add app bar

---
+++ src/components/AppBar.vue
@@ -0,0 +1,8 @@
+<template>
+    <!-- AppBar goes here -->
+    <v-app-bar app>
+      <v-toolbar-title>Crud App</v-toolbar-title>
+      <!-- Add any additional AppBar content here, like buttons or navigation links -->
+    </v-app-bar>
+
+</template>
---
+++ src/layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <AppBar/>
+
+    <!-- Main content -->
+    <v-main>
+      <router-view />
+    </v-main>
+
+    <!-- Footer -->
+    <AppFooter />
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+// Any necessary script setup can go here
+</script>
+
