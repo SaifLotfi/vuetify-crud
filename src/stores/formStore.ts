4187eaf64f9ed1fdd207b82069e9f8f6db9ebeeb
saif <saiflotfi@proton.me>
2024-11-06 11:08:22 +0200

    add id to form store

--- src/stores/formStore.ts
+++ src/stores/formStore.ts
@@ -3,10 +3,12 @@ import { defineStore } from "pinia";
 export const useFormStore = defineStore("form", () => {
   const form = ref(null);
   const valid = ref(false);
+  const id = ref<number|null>(null);
   const firstName = ref("");
   const lastName = ref("");
   const email = ref("");
   const salary = ref<number | null>(null);
   const isEdit = ref(false);
-  return { form, valid, firstName, lastName, email, salary,isEdit };
+
+  return { form, valid,id, firstName, lastName, email, salary, isEdit };
 });
