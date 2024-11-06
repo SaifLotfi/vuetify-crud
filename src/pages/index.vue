1f46d72d4454d57c86330ce519c4953c7c108f06
saif <saiflotfi@proton.me>
2024-11-06 11:08:49 +0200

    connect app to server

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -14,24 +14,38 @@ const employeeStore = useEmployeesStore();
 const formStore = useFormStore();
 
 const { employees } = storeToRefs(employeeStore);
-
 const { isEdit } = storeToRefs(formStore);
 
 const addEmployee = (employee: Employee) => {
   if (isEdit.value) {
-    const index = employees.value.findIndex((e: Employee) => e.email === employee.email);
+    const index = employees.value.findIndex(
+      (e: Employee) => e.email === employee.email
+    );
     employees.value.splice(index, 1, employee);
     isEdit.value = false;
   } else {
-    employees.value.push(employee);
+    try {
+      axios.post("http://localhost:5000/employees", {
+        ...employee,
+      });
+
+      employees.value.push(employee) 
+    } catch (error) {
+      console.log(error);
+    }
   }
 };
 
-const deleteEmployee = (email: string) => {
-  employees.value = employees.value.filter((e:Employee) => e.email !== email);
+const deleteEmployee = (id: number) => {
+  try {
+    axios.delete(`http://localhost:5000/employees/${id}`);
+  } catch (error) {
+    console.log(error);
+  }
+  employees.value = employees.value.filter((e: Employee) => e.id !== id);
 };
 
-onMounted(async() => {
+onMounted(async () => {
   try {
     const response = await axios.get("http://localhost:5000/employees");
     employees.value = response.data;
@@ -40,5 +54,4 @@ onMounted(async() => {
     console.log(error);
   }
 });
-
 </script>
